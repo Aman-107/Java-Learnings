e1bf69b28304df04d5d40181ed0ec942d42bd551
Aman <akthemaurya1999@gmail.com>
2025-01-09 12:20:04 +0530

    branch creation

---
+++ src/BranchInGit/CreationOfBranch.java
@@ -0,0 +1,11 @@
+package BranchInGit;
+
+public class CreationOfBranch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Branch 1 has been created");
+	}
+
+}
