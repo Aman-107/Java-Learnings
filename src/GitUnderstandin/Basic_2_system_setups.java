94de7cadbcf08a4ffcebe59dbe5e9e4851aae376
Aman <akthemaurya1999@gmail.com>
2025-01-09 11:30:34 +0530

    updation 2

---
+++ src/GitUnderstandin/Basic_2_system_setups.java
@@ -0,0 +1,13 @@
+package GitUnderstandin;
+
+public class Basic_2_system_setups {
+
+	public void main(String[] args) {
+		
+		System.out.println("Change in Base Code 1");
+		System.out.println("Change in Base Code 2");
+		System.out.println("Change in Base Code 3");
+		System.out.println("Change in Base Code 4");
+		
+	}
+}
