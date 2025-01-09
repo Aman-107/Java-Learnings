25bf88868adece90a68ae2254b330523b5ae8a6a
Aman <akthemaurya1999@gmail.com>
2025-01-09 12:02:36 +0530

    from cloned one

--- src/GitUnderstandin/Basic_2_system_setups.java
+++ src/GitUnderstandin/Basic_2_system_setups.java
@@ -19,6 +19,25 @@ public void changein () {
 		System.out.println("Change in Base Code 4");
 		
 		changein();
+		WebAutomation();
+		restAssured();
+	}
+	
+public void restAssured () {
+		
+		System.out.println("Change in restAssured Code 1");
+		System.out.println("Change in restAssured Code 2");
+		System.out.println("Change in restAssured Code 3");
+		System.out.println("Change in restAssured Code 4");
 		
 	}
+
+public void WebAutomation () {
+	
+	System.out.println("Change in WebAutomation Code 1");
+	System.out.println("Change in WebAutomation Code 2");
+	System.out.println("Change in WebAutomation Code 3");
+	System.out.println("Change in WebAutomation Code 4");
+	
+}
 }
