0b0c6fd67ce4f1eafcc04d4156c7e08d88c5b52a
Aman <akthemaurya1999@gmail.com>
2025-01-09 11:51:43 +0530

    base class change

--- src/GitUnderstandin/Basic_2_system_setups.java
+++ src/GitUnderstandin/Basic_2_system_setups.java
@@ -2,12 +2,23 @@ package GitUnderstandin;
 
 public class Basic_2_system_setups {
 
+public void changein () {
+		
+		System.out.println("Change in class Code 1");
+		System.out.println("Change in class Code 2");
+		System.out.println("Change in class Code 3");
+		System.out.println("Change in class Code 4");
+		
+	}
+	
 	public void main(String[] args) {
 		
 		System.out.println("Change in Base Code 1");
 		System.out.println("Change in Base Code 2");
 		System.out.println("Change in Base Code 3");
 		System.out.println("Change in Base Code 4");
+		
+		changein();
 		
 	}
 }
